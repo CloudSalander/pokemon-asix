25430af461041048a458200e95e43e7ca6516676
Cloud <ruben.as532@gmail.com>
2023-02-17 10:12:00 +0100

    First Pokemon Version

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Benviguts a Pokemon ASIX!");
+        Pokemon p1 = new Pokemon();
+        p1.attack();
+    }
+}
