8f5acfca3d5259a4c4ad08a8ff4d9e1b4528dc6c
Cloud <ruben.as532@gmail.com>
2023-03-10 10:02:28 +0100

    Removed original files

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Benviguts a Pokemon ASIX!");
-        Pokemon p1 = new Pokemon();
-        p1.attack();
-    }
-}
